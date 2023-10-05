1e6ad760d78910914618bccbe89cd19a95a6dc75
MatteDev02 <matteolambertucci3@gmail.com>
2023-10-05 19:36:56 +0200

    add screens and src folfer

---
+++ src/screens/Signin.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const SignInScreen = () => {
+	return (
+		<View>
+			<Text>SignInScreen</Text>
+		</View>
+	);
+};
+
+export default SignInScreen;
---
+++ src/screens/index.js
@@ -0,0 +1,5 @@
+import HomeScreen from "./Home.js";
+import SignInScreen from "./signin.js";
+import SignUpScreen from "./signup.js";
+
+export { HomeScreen, SignInScreen, SignUpScreen };
